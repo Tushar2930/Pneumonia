c1e78fd2fb139f36ee52ab38f04c049e9678895f
Tushar <tusharshingane1234@gmail.com>
2023-04-07 09:44:41 +0530

    initial

--- app.py
+++ app.py
@@ -1,52 +1,59 @@
-from flask import Flask, render_template, request, jsonify, send_file, make_response, redirect, flash
-import os
-import cv2
+import streamlit as st
+import tensorflow as tf
 import numpy as np
-import pickle
-from tensorflow.keras.models import load_model
-import keras.utils as image
+import cv2
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+import tensorflow_hub as hub
 
 
-# covid_model = pickle.load(open('covid19_model.pkl', 'rb'))
-covid_model = load_model('model.h5')
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 
-UPLOAD_FOLDER = './assets/uploads'
-app = Flask(__name__)
+@st.cache()
+def load_model():
+    model = tf.keras.models.load_model("Pneumonia_model.h5", custom_objects={
+                                       'KerasLayer': hub.KerasLayer})
+    return model
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def app():
+    # load model and deploy it predicts whether user has covid or not by taking image as input
+    model = load_model()
+    st.title("Pneumonia Detection")
+    st.write(
+        "Upload an image of your chest X-ray to detect whether you have Pneumonia or not")
+    file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
+    if file is not None:
 
+        image = tf.keras.preprocessing.image.load_img(
+            file, target_size=(224, 224))
+        image = tf.keras.preprocessing.image.img_to_array(image)
+        img = image/255.0
+        disease_class = ['Covid-19', 'Non Covid-19']
+        custom = model.predict(img)
+        st.write(custom[0])
 
-@app.route('/hospital/covid')
-def covid():
-    return render_template('form.html')
+        a = custom[0]
+        ind = np.argmax(a)
 
-
-def predict_cancer(file_path):
-    img = cv2.imread(file_path)
-    img = cv2.resize(img, (64, 64))
-    disease_class = ['Covid-19', 'Non Covid-19']
-    img = img/255.0
-    x = np.expand_dims(img, axis=0)
-    custom = covid_model.predict(x)
-    print(custom[0])
-    a = custom[0]
-    ind = np.argmax(a)
-    print('Prediction:', disease_class[ind], 'with probability of ', np.max(
-        custom[0])*100, '%')
-    os.remove(file_path)
-    return jsonify({'prediction': disease_class[ind], 'probability': np.max(custom[0])*100})
-
-
-@app.route('/predict_covid', methods=['POST'])
-def predict():
-    f = request.files['file']
-    f.save(os.path.join(UPLOAD_FOLDER, f.filename))
-    return predict_cancer("./assets/uploads/" + f.filename)
+        st.write('Prediction:', disease_class[ind], 'with probability of ', np.max(
+            custom[0])*100, '%')
+        # image = tf.keras.preprocessing.image.load_img(
+        #     file, target_size=(224, 224))
+        # st.image(image, caption='Uploaded Image.', use_column_width=True)
+        # st.write("")
+        # st.write("Classifying...")
+        # image = tf.keras.preprocessing.image.img_to_array(image)
+        # image = tf.keras.applications.mobilenet.preprocess_input(image)
+        # image = tf.expand_dims(image, 0)
+        # prediction = model.predict(image)
+        # if prediction == 0:
+        #     st.write("You have Covid-19")
+        # else:
+        #     st.write("You don't have Covid-19")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app()
